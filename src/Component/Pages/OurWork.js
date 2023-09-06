a9e4ddd1423f44b24d486f8db7ad222f38a8200a
shadab choudhary <140692984+reversethought-shadab@users.noreply.github.com>
2023-09-06 09:42:39 +0530

    Update OurWork.js

--- src/Component/Pages/OurWork.js
+++ src/Component/Pages/OurWork.js
@@ -2567,7 +2567,7 @@ const OurWork = (props) => {
                     />
                     <div className="project_overview">
                       <div className="top_overview">
-                        <h2 className="proj_name text-black">AYM Leaf Lets</h2>
+                        <h2 className="proj_name text-black">AYM Leaflets</h2>
                         <p className="proj_type text-black">Creative Design</p>
                       </div>
                       <div className="bottom_overview">
