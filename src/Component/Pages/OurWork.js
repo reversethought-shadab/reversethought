1cab26b14da6bace705398323f1962be0521167a
shadab choudhary <140692984+reversethought-shadab@users.noreply.github.com>
2023-09-06 09:32:52 +0530

    Update OurWork.js

--- src/Component/Pages/OurWork.js
+++ src/Component/Pages/OurWork.js
@@ -477,7 +477,7 @@ const OurWork = (props) => {
 
               <div className="projectsGalleryR">
                 <a
-                  href="/welspunGroup"
+                  href="/Welpsun"
                   onMouseEnter={props.onMouseEnter}
                   onMouseLeave={props.onMouseLeave}
                 >
